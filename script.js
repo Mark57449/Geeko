d5fb45857bfdf2693115f57631975d2e1a51b336
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-07 12:04:30 -0300

    Movido para a pasta ASSETS.

--- script.js
+++
@@ -1,6 +0,0 @@
-import {MDCRipple} from '@material/ripple';
-
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
-
-import * as mdc from 'material-components-web';
-const checkbox = new mdc.checkbox.MDCCheckbox(document.querySelector('.mdc-checkbox'));
