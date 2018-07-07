73ca4a0ddbd1f04b8c7c76f58ce85ffa0fc37119
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-07 12:23:41 -0300

    Conversão do arquivo post-view.html para .php

---
+++ post-view.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Postagem - Geeko - Material Design</title>
+  <link rel="shortcut icon" href="https://material.io/favicon.ico">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Roboto:300,400,500,700|Material+Icons|PT+Serif">
+  <link rel="stylesheet" href="node_modules/bootstrap/compiler/bootstrap.css">
+  <link rel="stylesheet" href="ASSETS/STYLE/style.css">
+  <link rel="stylesheet" href="ASSETS/STYLE/header.css">
+  <link rel="stylesheet" href="ASSETS/STYLE/post.css">
+  <link rel="stylesheet" href="ASSETS/STYLE/footer.css">
+</head>
+  <body class="margin-body">
+    <header id="header">
+      <nav class="navbar fixed-top navbar-expand-lg p-0 shadow bg-1">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="ASSETS/Branding/Geeko!SVG.svg" width="125px" alt="Logotipo Geeko!"><h2 class="now m-0"><span class="barra">#</span><span class="font">Agora</span></h2>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse go-menu-cat" id="navbarSupportedContent">
+            <nav id="basic-tab-bar" class="mdc-tab-bar">
+              <a class="mdc-tab" href="#one">
+                <i class="material-icons animes a">stars</i>Animes
+                <span class="mdc-tab__indicator a"></span>
+              </a>
+              <a class="mdc-tab" href="#two">
+                <i class="material-icons games b">videogame_asset</i>Games
+                <span class="mdc-tab__indicator b"></span>
+              </a>
+              <a class="mdc-tab" href="#three">
+                <i class="material-icons series c">cast</i>Séries
+                <span class="mdc-tab__indicator c"></span>
+              </a>
+              <a class="mdc-tab" href="#three">
+                <i class="material-icons filmes d">weekend</i>Filmes
+                <span class="mdc-tab__indicator d"></span>
+              </a>
+              <a class="mdc-tab mdc-tab--active" href="#three">
+                <i class="material-icons hqm e">library_books</i>HQ/Mangás
+                <span class="mdc-tab__indicator e"></span>
+              </a>
+              <div class="go-btn-pesquisa">
+                <i class="material-icons fix-icon" style="cursor: pointer;">search</i>
+              </div>
+            </nav>
+            LOJA GEEKO SERA A MELHOR!
+          </div>
+        </div>
+      </nav>
+      <!-- <nav id="extra-navbar" class="navbar navbar-expand-lg p-0">
+        <div class="container">
+          <h3><i class="material-icons fix-icon">local_activity</i> Tópicos Recentes: </h3>
+          <a href="#" class="badge badge-secondary yeah-100">#Comic Con</a>
+          <a href="#" class="badge badge-secondary yeah-100">#Anime EXP</a>
+          <a href="#" class="badge badge-secondary yeah-100">#Anime Friends</a>
+          <a href="#" class="badge badge-secondary yeah-75">#Dorei-ku</a>
+          <a href="#" class="badge badge-secondary yeah-75">#Nanatsu no Taizai</a>
+          <a href="#" class="badge badge-secondary yeah-75">#Comic Con</a>
+          <a href="#" class="badge badge-secondary yeah-75">#Anime EXP</a>
+          <a href="#" class="badge badge-secondary yeah-50">#Anime Friends</a>
+          <a href="#" class="badge badge-secondary yeah-50">#Dorei-ku</a>
+
+        </div>
+      </nav> -->
+    </header>
+    <main id="post-view">
+      <div class="container">
+        <div class="mdc-layout-grid">
+          <section class="mdc-layout-grid__inner">
+            <article class="mdc-layout-grid__cell--span-8 post mx-auto">
+              <div class="intro-post">
+                <div id="local"><a href="#">Página Inicial</a> > <a href="#">Anime</a> > <a href="#">Miku is Given a Vintage Twist With New Project DIVA Figure!</a></div>
+                <h1>Miku is Given a Vintage Twist With New Project DIVA Figure!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div id="info">
+                  <span class="anime">One Piece</span>
+                  <span class="anime">One Piece</span>
+                  <span class="anime">One Piece</span>
+                  <span class="data"><i class="material-icons">date_range</i>31 de Janeiro</span>
+                  <span class="leitura"><i class="material-icons">access_time</i>5 Minutinhos de Leitura</span>
+                </div>
+              </div>
+              <div class="thumbpost" style="background: url('ASSETS/IMG/Anime2.jpg');"></div>
+              Showing off her sweet smile and beautiful Vintage Dress, Max Factory's new 1/7 scale Hatsune Miku figure is sure to get fans pumped for her next concert!<br><br>
+
+              The highlight of this figure is obviously the shiny and stylish costume, which is the Vintage Dress module design from Hatsune Miku -Project DIVA- F 2nd! The pearly paintwork adds a romantic feel to the atmosphere, and with the crystal microphone in her hand, it’s easy to imagine her up on stage performing.<br><br><br>
+
+
+
+              Her heartwarming smile is also sure to steal fans' hearts!<br><br><br>
+
+
+              Bring the concert to your home by preordering at Tokyo Otaku Mode, with an estimated release of Jul. 2019.<br><br>
+
+              Product Details<br>
+              Name: Hatsune Miku: Vintage Dress Ver. 1/7 Scale Figure<br>
+              Series: Hatsune Miku -Project DIVA- F 2nd<br>
+              Product Line: 1/7th Scale<br>
+              Manufacturer: Max Factory<br>
+              Sculptor: Grizzly Panda
+              Specifications: Painted 1/7th scale ABS & PVC product with stand included<br>
+              Height (approx.): 250 mm | 9.8"<br>
+              Modular design: Sakurau / Alice<br><br>
+
+              ©SEGA / © Crypton Future Media, INC. www.piapro.net<br><br>
+              <div id="extra-info" class="p-3">
+                <div class="row">
+                  <div class="col-6">
+                    <div class="ibagens" style="background: url('ASSETS/IMG/Eu2.png');"></div>
+                    <div class="aaa">
+                      <span class="font-weight-bold a">Wendel Henrique</span>
+                      <span class="font-weight-normal b text-muted">O Pseudo-lolicon!</span>
+                    </div>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                  </div>
+                  <div class="col-6 fonte">
+                    <h4 class="font-weight-bold">Fontes Bibliográficas:</h4>
+                    <a href="#">https://www.geeko.com.br</a>
+                    <a href="#">https://www.geeko.com.br</a>
+                    <a href="#">https://www.geeko.com.br</a>
+                    <a href="#">https://www.geeko.com.br</a>
+                    <a href="#">https://www.geeko.com.br</a>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </section>
+        </div>
+      </div>
+    </main>
+    <footer id="footer" class="navbar bg">
+      <div class="container-fluid">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        lorem
+          <span><img src="ASSETS/Branding/Geeko!SVG.svg" width="125px" alt="Logotipo Geeko!"> (c) 2018 Copyright Holder All Rights Reserved.</span>
+          <span class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</span>
+      </div>
+    </footer>
+    <!-- JQuery & Popper & Bootstrap 4-->
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+  </body>
+</html>
