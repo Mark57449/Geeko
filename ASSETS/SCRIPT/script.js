c8499d42122ee865627ec10b8561835f57d2dcab
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-07 12:11:31 -0300

    Pasta criada para os arquivos .js

---
+++ ASSETS/SCRIPT/script.js
@@ -0,0 +1,10 @@
+import {MDCRipple} from '@material/ripple';
+
+const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+
+import * as mdc from 'material-components-web';
+const checkbox = new mdc.checkbox.MDCCheckbox(document.querySelector('.mdc-checkbox'));
+
+import {MDCChipSet} from '@material/chips';
+
+const chipSet = new MDCChipSet(document.querySelector('.mdc-chip-set'));
