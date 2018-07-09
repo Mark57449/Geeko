582a67e7d069ab61b360fe15bb055cb6832cc557
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-09 16:00:21 -0300

    Código base e meta-tags.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Postagem - Geeko - Material Design</title>
+  <link rel="shortcut icon" href="https://material.io/favicon.ico">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato|Roboto:300,400,500,700|Material+Icons|PT+Serif">
+  <link rel="stylesheet" href="node_modules/bootstrap/compiler/bootstrap.css">
+  <link rel="stylesheet" href="ASSETS/STYLE/style.css">
+  <link rel="stylesheet" href="ASSETS/STYLE/header.css">
+  <link rel="stylesheet" href="ASSETS/STYLE/post.css">
+  <link rel="stylesheet" href="ASSETS/STYLE/footer.css">
+</head>
+<body class="margin-body">
+ <?php
+  include('ELEMENTS/PerfilEFontes.php');
+ ?>
+</body>
+<!-- JQuery & Popper & Bootstrap 4-->
+<script src="node_modules/jquery/dist/jquery.js"></script>
+<script src="node_modules/popper.js/dist/umd/popper.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+</html>
