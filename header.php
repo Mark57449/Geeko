4366bf8278af7b2260109c7f77296a4d196cfe98
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-09 15:59:15 -0300

    Header do Blog Geeko

---
+++ header.php
@@ -0,0 +1,40 @@
+<header id="header">
+  <nav class="navbar fixed-top navbar-expand-lg p-0 shadow bg-1">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="ASSETS/Branding/Geeko!SVG.svg" width="125px" alt="Logotipo Geeko!"><h2 class="now m-0"><span class="barra">#</span><span class="font">Agora</span></h2>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse go-menu-cat" id="navbarSupportedContent">
+        <nav id="basic-tab-bar" class="mdc-tab-bar">
+          <a class="mdc-tab" href="#one">
+            <i class="material-icons animes a">stars</i>Animes
+            <span class="mdc-tab__indicator a"></span>
+          </a>
+          <a class="mdc-tab" href="#two">
+            <i class="material-icons games b">videogame_asset</i>Games
+            <span class="mdc-tab__indicator b"></span>
+          </a>
+          <a class="mdc-tab" href="#three">
+            <i class="material-icons series c">cast</i>Séries
+            <span class="mdc-tab__indicator c"></span>
+          </a>
+          <a class="mdc-tab" href="#three">
+            <i class="material-icons filmes d">weekend</i>Filmes
+            <span class="mdc-tab__indicator d"></span>
+          </a>
+          <a class="mdc-tab mdc-tab--active" href="#three">
+            <i class="material-icons hqm e">library_books</i>HQ/Mangás
+            <span class="mdc-tab__indicator e"></span>
+          </a>
+          <div class="go-btn-pesquisa">
+            <i class="material-icons fix-icon" style="cursor: pointer;">search</i>
+          </div>
+        </nav>
+        LOJA GEEKO SERA A MELHOR!
+      </div>
+    </div>
+  </nav>
+</header>
