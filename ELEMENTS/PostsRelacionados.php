762eb85f8de939148a40f833fd3b0a7cd2fd10a0
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-10 19:34:28 -0300

    Área de Postagens Relacionadas ou Sugeridas, com um Titulo Principal, Subtitulo interativo, Imagem com titulo e Link.

---
+++ ELEMENTS/PostsRelacionados.php
@@ -0,0 +1,14 @@
+<section id="PostsRelacionados" class="bg-white container shadow-sm">
+  <h3 class="font-weight-bold">Artigos Relacionados</h3>
+  <h5 class="text-muted subtitulo font-weight-normal">As montanhas te dizem que há algo relacionado.</h5>
+  <div class="row container-fluid">
+    <?php for ($i=0; $i < 4; $i++) { ?>
+    <article class="col-3">
+      <a href="#">
+        <div class="thumbpost" style="background: url('ASSETS/IMG/Anime2.jpg');"></div>
+        <h4 align="left">Postagem sobre tal assunto</h4>
+      </a>
+    </article>
+    <?php } ?>
+  </div>
+</section>
---
+++ ELEMENTS/SugestoesDePosts.php
@@ -0,0 +1,13 @@
+<section id="SugestoesDePosts" class="bg-white container shadow-sm">
+  <h3 class="font-weight-bold">Sugestões de Postagens</h3>
+  <div class="row container-fluid">
+    <?php for ($i=0; $i < 4; $i++) { ?>
+     <article class="col-3">
+      <a href="#">
+        <div class="thumbpost" style="background: url('ASSETS/IMG/Anime2.jpg');"></div>
+        <h4 align="left">Postagem sobre tal assunto</h4>
+      </a>
+    </article>
+    <?php } ?>
+  </div>
+</section>
