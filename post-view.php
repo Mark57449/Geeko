b9872633893c2af8c0ff809c6187fe4f61a72db8
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-10 19:15:37 -0300

    Adição: Sombra, Col-9, Data de postagem e tempo de leitura empurrado para esquerda, Texto de exemplo Lorem Ipsum, Postagens Relacionadas, Sugestões de Posts.

--- post-view.php
+++ post-view.php
@@ -1,48 +1,33 @@
 
 
     <main id="post-view">
-      <div class="container">
-        <div class="mdc-layout-grid">
-          <section class="mdc-layout-grid__inner">
-            <article class="mdc-layout-grid__cell--span-8 post mx-auto">
+      <div class="container bg-white shadow-sm">
+          <section class="row">
+            <article class="col-9 post mx-auto">
               <div class="intro-post">
                 <div id="local"><a href="#">Página Inicial</a> > <a href="#">Anime</a> > <a href="#">Miku is Given a Vintage Twist With New Project DIVA Figure!</a></div>
                 <h1>Miku is Given a Vintage Twist With New Project DIVA Figure!</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <div id="info">
-                  <span class="anime">One Piece</span>
-                  <span class="anime">One Piece</span>
-                  <span class="anime">One Piece</span>
-                  <span class="data"><i class="material-icons">date_range</i>31 de Janeiro</span>
-                  <span class="leitura"><i class="material-icons">access_time</i>5 Minutinhos de Leitura</span>
+                  <span class="anime">Anime</span>
+                  <span class="anime">Lançamentos de 2018</span>
+                  <span class="data text-muted"><i class="material-icons">date_range</i>Segunda-feira, 9 de julho de 2018</span>
+                  <span class="leitura text-muted"><i class="material-icons">access_time</i>5 Minutinhos de Leitura</span>
                 </div>
               </div>
               <div class="thumbpost" style="background: url('ASSETS/IMG/Anime2.jpg');"></div>
-              Showing off her sweet smile and beautiful Vintage Dress, Max Factory's new 1/7 scale Hatsune Miku figure is sure to get fans pumped for her next concert!<br><br>
-
-              The highlight of this figure is obviously the shiny and stylish costume, which is the Vintage Dress module design from Hatsune Miku -Project DIVA- F 2nd! The pearly paintwork adds a romantic feel to the atmosphere, and with the crystal microphone in her hand, it’s easy to imagine her up on stage performing.<br><br><br>
-
-
-
-              Her heartwarming smile is also sure to steal fans' hearts!<br><br><br>
-
-
-              Bring the concert to your home by preordering at Tokyo Otaku Mode, with an estimated release of Jul. 2019.<br><br>
-
-              Product Details<br>
-              Name: Hatsune Miku: Vintage Dress Ver. 1/7 Scale Figure<br>
-              Series: Hatsune Miku -Project DIVA- F 2nd<br>
-              Product Line: 1/7th Scale<br>
-              Manufacturer: Max Factory<br>
-              Sculptor: Grizzly Panda
-              Specifications: Painted 1/7th scale ABS & PVC product with stand included<br>
-              Height (approx.): 250 mm | 9.8"<br>
-              Modular design: Sakurau / Alice<br><br>
-
-              ©SEGA / © Crypton Future Media, INC. www.piapro.net<br><br>
-              <?php  ?>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <?php include 'ELEMENTS/PerfilEFontes.php'; ?>
             </article>
           </section>
         </div>
-      </div>
     </main>
+    <?php
+      include 'ELEMENTS/PostsRelacionados.php';
+      include 'ELEMENTS/SugestoesDePosts.php';
+     ?>
