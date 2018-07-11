48d32e49927979981d0485ca3b029d5e0211ed21
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-10 21:53:33 -0300

    Breve descrição abaixo do titulo removida.

--- post-view.php
+++ post-view.php
@@ -7,7 +7,6 @@
               <div class="intro-post">
                 <div id="local"><a href="#">Página Inicial</a> > <a href="#">Anime</a> > <a href="#">Miku is Given a Vintage Twist With New Project DIVA Figure!</a></div>
                 <h1>Miku is Given a Vintage Twist With New Project DIVA Figure!</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <div id="info">
                   <span class="anime">Anime</span>
                   <span class="anime">Lançamentos de 2018</span>
