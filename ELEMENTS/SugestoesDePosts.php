cd2af7e1896b61c259ae914cebed201f928de7ce
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-10 21:57:50 -0300

    Cor de Link alterada para preta, subtitulo e exemplo do mesmo foi adicionado.

--- ELEMENTS/SugestoesDePosts.php
+++ ELEMENTS/SugestoesDePosts.php
@@ -1,5 +1,6 @@
 <section id="SugestoesDePosts" class="bg-white container shadow-sm">
   <h3 class="font-weight-bold">Sugestões de Postagens</h3>
+  <h5 class="text-muted subtitulo font-weight-normal">As montanhas te dizem que há algo relacionado.</h5>
   <div class="row container-fluid">
     <?php for ($i=0; $i < 4; $i++) { ?>
      <article class="col-3">
