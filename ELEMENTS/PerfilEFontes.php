a54c3dfa087302de939af7f581842f1d84c826da
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-07-10 21:55:34 -0300

    Desenvolvimento de área de redes sociais, estrutura modificada para 2 colunas, tamanhos de titulo e biografia modificados.

--- ELEMENTS/PerfilEFontes.php
+++ ELEMENTS/PerfilEFontes.php
@@ -1,13 +1,17 @@
 <div id="extra-info" class="p-3">
   <div class="row">
-    <div class="col-6">
-      <div class="ibagens" style="background: url('ASSETS/IMG/Eu2.png');"></div>
-      <div class="aaa">
+      <div class="ibagens col-3" style="background: url('ASSETS/IMG/Eu2.png');"></div>
+      <div class="aaa col-9">
         <span class="font-weight-bold a">Wendel Henrique</span>
-        <span class="font-weight-normal b text-muted">O Pseudo-lolicon!</span>
-      </div>
-      <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <span class="font-weight-normal b text-muted">O Pseudo-lolicon! | O Criador</span>
+
+      <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <a href="#" style="display:block;">Leia mais...</a>
+      </p>
+      <div class="Social"></div>
     </div>
+  </div>
+    <div class="row">
     <div class="col-6 fonte">
       <h5 class="font-weight-bold">Fontes Bibliográficas:</h5>
       <a href="#">https://www.geeko.com.br</a>
